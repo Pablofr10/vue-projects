e6611a310412bc4a9a0a00e8935977eb6aa8d7f6
Pablo Araújo <pablo.oliveira@animaeducacao.com>
2021-01-18 23:09:45 -0300

    Adicionando calendário.

---
+++ src/components/Calendar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="m-auto">
+    <h1 class="text-2xl my-2 text-center">Vue Calendar</h1>
+    {{}}
+    <section class="mx-2 flex justify-between">
+      <h2 class="font-bold">{{ currentMonthName }}</h2>
+      <h2 class="font-bold">{{ currentYear }}</h2>
+    </section>
+    <section class="flex">
+      <p
+        class="text-center"
+        style="width: 14.28%"
+        v-for="day in days"
+        :key="day"
+      >
+        {{ day }}
+      </p>
+    </section>
+    <section class="flex flex-wrap">
+      <p
+        class="text-center my-2"
+        style="width: 14.28%"
+        v-for="num in daysInMonth(currentYear, currentMonth)"
+        :key="num"
+      >
+        {{ num }}
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentMonth: new Date().getMonth() + 1,
+      currentMonthName: new Date().toLocaleString("default", { month: "long" }),
+      currentYear: new Date().getFullYear(),
+      days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    };
+  },
+  methods: {
+    daysInMonth(year, month) {
+      return new Date(year, month, 0).getDate();
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav
+    class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px- py-2"
+  >
+    <a>Heroes</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Vue Projects",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
